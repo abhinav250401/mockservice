e6b4001c1090718b18fdd70d7b1db9da13c52697
abhinav250401 <abhinav.250401@gmail.com>
2026-04-26 20:48:44 +0530

    Inital Commit

---
+++ src/test/java/project/mockservice/MockserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.mockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
